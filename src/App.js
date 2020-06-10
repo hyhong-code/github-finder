4c0fac36df535762cb7394398c15fb04df84501f
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-10 16:35:09 -0700

    add NotFound component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
 import Navbar from "./components/layout/Navbar";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
 import User from "./components/users/User";
-import { connect } from "react-redux";
+import NotFound from "./components/pages/NotFound";
 import Home from "./components/pages/Home";
+
 import PropTypes from "prop-types";
 import "./App.css";
 
@@ -19,6 +21,7 @@ const App = ({ alert }) => {
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
           <Route exact path="/user/:login" component={User} />
+          <Route component={NotFound} />
         </Switch>
       </div>
     </div>
